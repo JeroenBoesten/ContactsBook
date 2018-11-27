fba36b66e37f5ef24a60c4f45baa7864c927670d
DESKTOP-Boesten,Jeroen <Jeroen@JeroenOnline.biz>
2018-11-27 15:49:32 +0100

    Static template design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,98 +1,66 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+@extends('layouts.app')
+@section('title', 'Contacts')
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            <div class="d-flex justify-content-between align-items-center">
+                <h1>Contacts</h1>
+                <button class="btn btn-primary"><i class="fas fa-plus"></i></button>
             </div>
         </div>
-    </body>
-</html>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-striped text-left">
+                    <thead>
+                    <tr>
+                        <th scope="col" style="max-width: 75px;">A</th>
+                        <th></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>
+                            <div class="row">
+                                <div class="col-xs-5 col-sm-4 col-md-2">
+                                    <img src="https://www.gravatar.com/avatar/04e9be03ee50d2b21da3cea5b58cdc1a?s=75&d=mp" alt="Debbie Schmidt" class="img-fluid rounded-circle d-block">
+                                </div>
+                                <div class="col-xs-7 col-sm-8 col-md-10">
+                                    <h4>Mark <b>Andersson</b></h4>
+                                    <address class="ml-3">
+                                        <i class="fas fa-phone"></i> +316 46054195<br>
+                                        <i class="fas fa-envelope-open"></i> +316 46054195<br>
+                                        <i class="fab fa-twitter"></i> +316 46054195<br>
+                                    </address>
+                                </div>
+                            </div>
+                        </td>
+                        <td class="align-middle text-right">
+                            <button class="btn btn-secondary"><i class="fas fa-pencil-alt"></i> </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div class="row">
+                                <div class="col-xs-5 col-sm-4 col-md-2">
+                                    <img src="https://www.gravatar.com/avatar/04e9be03ee50d2b21da3cea5b58cdc1a?s=75&d=mp" alt="Debbie Schmidt" class="img-fluid rounded-circle d-block">
+                                </div>
+                                <div class="col-xs-7 col-sm-8 col-md-10">
+                                    <h4>Mark <b>Andersson</b></h4>
+                                    <address class="ml-3">
+                                        <i class="fas fa-phone"></i> +316 46054195<br>
+                                        <i class="fas fa-envelope-open"></i> +316 46054195<br>
+                                        <i class="fab fa-twitter"></i> +316 46054195<br>
+                                    </address>
+                                </div>
+                            </div>
+                        </td>
+                        <td class="align-middle text-right">
+                            <button class="btn btn-secondary"><i class="fas fa-pencil-alt"></i> </button>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
